4a7c5d132b2ca8dfadc2b74bf38227bdb3518914
darkicewolf50 <brock.tomlinson@ucalgary.ca>
2023-03-13 15:29:53 -0600

    Fixed the problem for 2 questions fix it so that integer division happens instead

--- ex3.1.py
+++ ex3.1.py
@@ -28,8 +28,7 @@ class Stack:
 # Define a stack-based interpreter function
 def evaluate(expression):
     stack = Stack()
-    for token in expression.split():
-        print(f'print1 {token}')
+    for token in expression:
         if token in operators:
             # Push the operator onto the stack
             stack.push(token)
@@ -39,11 +38,8 @@ def evaluate(expression):
         elif token == ')':
             # Pop the top operator and the two top operands from the stack
             operand1 = stack.pop()
-            print(operand1)
             operand2 = stack.pop()
-            print(operand2)
             operator = stack.pop()
-            print(operator)
             # Apply the operator to the operands and push the result onto the stack
             result = operators[operator](operand1, operand2)
             stack.push(result)
@@ -57,10 +53,7 @@ def evaluate(expression):
 # Get the expression from the command line argument
 expression = sys.argv[1]
 
-# Remove the first and last parentheses
-parsed_expression = expression[1:-1].split()
-
 # Evaluate the expression and print the result
-result = evaluate(' '.join(parsed_expression))
+result = evaluate(expression)
 print(f'result: {result}')
 
